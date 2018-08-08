c04a35554be5cb9266075d883c1ba1850814aa0f
TranLongPC <tranlongpc@gmail.com>
2018-08-08 16:02:41 +0700

    Initial commit

---
+++ tests/Models/CustomActivityModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Spatie\Activitylog\Test\Models;
+
+use Spatie\Activitylog\Models\Activity;
+
+class CustomActivityModel extends Activity
+{
+    public function getCustomPropertyAttribute()
+    {
+        return $this->changes();
+    }
+}
